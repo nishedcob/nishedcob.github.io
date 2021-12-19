4342fdb82a0dd81a29609597edce1ccb2a56c5f8
Nicholas Spalding <nspalding@ioet.com>
2021-12-19 04:06:07 -0500

    Refactor templater.sh to template out LaTeX documents as well as Markdown documents

--- templater.sh
+++ templater.sh
@@ -4,12 +4,57 @@ pip_install() {
     pip install -r requirements.txt
 }
 
+generic_build() {
+    TEMPLATE=$1
+    OUTPUT=$2
+    LANG=$3
+    ninja2 -j $TEMPLATE < <(yq '.' <(cat universal.yaml ${LANG}.yaml)) > $OUTPUT
+}
+
+generic_md_build() {
+    OUTPUT=$1
+    LANG=$2
+    generic_build index.md.j2 ${OUTPUT}.md $LANG
+}
+
+generic_latex_build() {
+    OUTPUT=$1
+    LANG=$2
+    generic_build main.tex.j2 ${OUTPUT}.tex $LANG
+}
+
+build_en_md() {
+    generic_md_build index en
+}
+
+build_es_md() {
+    generic_md_build es/index es
+}
+
+build_en_web() {
+    build_en_md
+}
+
+build_es_web() {
+    build_es_md
+}
+
+build_en_latex() {
+    generic_latex_build main en
+}
+
+build_es_latex() {
+    generic_latex_build es/main es
+}
+
 build_en() {
-    ninja2 -j index.md.j2 < <(yq '.' <(cat universal.yaml en.yaml)) > index.md
+    build_en_web
+    build_en_latex
 }
 
 build_es() {
-    ninja2 -j index.md.j2 < <(yq '.' <(cat universal.yaml es.yaml)) > es/index.md
+    build_es_web
+    build_es_latex
 }
 
 build_all() {
